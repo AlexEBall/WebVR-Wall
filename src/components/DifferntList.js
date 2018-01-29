686709feeae56f3c1370b2ac1b534a5d2e184db1
Alex Edward Ball <ballalexedward@gmail.com>
2018-01-28 19:02:17 -0800

    need to style each block with nice background properties and big letter

--- src/components/DifferntList.js
+++ src/components/DifferntList.js
@@ -3,11 +3,21 @@ import React from 'react';
 const DifferentList = ({characters}) => 
 <div className="list">
     {characters.map(item => 
-    <div className="list-row" key={item.id}>
-        <h2 className="heading">Name: {item.name}</h2>
-        <img src={item.image} alt="zelda character" />
+    <div 
+    className={`list-row list-row-${item.id}`} 
+    key={item.id}
+    style={{background: `url(${item.backgroundImg})`, backgroundSize: 'cover' }}
+    >
+        <div className={`slide__text-wrapper slide--${item.id}__text-wrapper`}>
+            <div className={`slide__letter slide--${item.id}__letter`}>
+                {(item.name).charAt(0)}
+                    <h2 className="heading">{item.name}></h2>
+                    <img src={item.image} alt="zelda character" />
+            </div>
+        </div>
     </div>
     )}
 </div>
+
 
 export default DifferentList;
