9fdfbf3281909c1f3d8f32bb8e37488cadaa395b
Alex Edward Ball <ballalexedward@gmail.com>
2018-01-28 19:15:02 -0800

    div structure alright, now need letters to show

--- src/components/DifferntList.js
+++ src/components/DifferntList.js
@@ -6,13 +6,19 @@ const DifferentList = ({characters}) =>
     <div 
     className={`list-row list-row-${item.id}`} 
     key={item.id}
-    style={{background: `url(${item.backgroundImg})`, backgroundSize: 'cover' }}
     >
-        <div className={`slide__text-wrapper slide--${item.id}__text-wrapper`}>
-            <div className={`slide__letter slide--${item.id}__letter`}>
-                {(item.name).charAt(0)}
-                    <h2 className="heading">{item.name}></h2>
-                    <img src={item.image} alt="zelda character" />
+        <div 
+        className="slide"
+        style={{background: `url(${item.backgroundImg})`, backgroundSize: 'cover' }}
+        >
+            <div className="slide__darkbg">
+                <div className={`slide__text-wrapper slide--${item.id}__text-wrapper`}>
+                    <div className={`slide__letter slide--${item.id}__letter`}>
+                        {(item.name).charAt(0)}
+                            <h2 className="heading">{item.name}</h2>
+                            <img src={item.image} alt="zelda character" />
+                    </div>
+                </div>
             </div>
         </div>
     </div>
