edf1bbe0ff2e8d9f94a7c107ba94084eed1d5997
Mariam Joan <royalton@gmail.com>
2018-02-01 19:22:05 -0800

    new

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import VRWorld from './pages/VRWorld';
+
+const App = () => 
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/VR-World/:id" component={VRWorld} />
+      </Switch>
+    </div>
+  </Router>;
+
+export default App;
