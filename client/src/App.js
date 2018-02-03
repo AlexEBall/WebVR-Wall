5e264f32603a12e0e631b7666828c9a8e67a9dfd
Alex Edward Ball <ballalexedward@gmail.com>
2018-02-03 12:01:32 -0800

    added water scene

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import VRWorld from './pages/VRWorld';
 import VRTry from './pages/VRTry';
+import VRWater from './pages/Water';
 
 export default class App extends Component { 
   
@@ -12,7 +13,7 @@ export default class App extends Component {
         <div className="topLevel">
           <Switch>
             <Route onUpdate={() => window.scrollTo(0, 0)} exact path="/" component={Home} />
-            <Route exact path="/VR-World/:id" component={VRWorld} />
+            <Route exact path="/VR-World/:id" component={VRWater} />
           </Switch>
         </div>
       </Router>
---
+++ client/src/pages/Water.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import 'aframe';
+// import {Entity, Scene} from 'aframe-react';
+import {Link} from 'react-router-dom';
+// import API from '../../utils/API';
+import character from './character.json';
+// import model from './model.obj';
+// import material from './materials.mtl';
+import $ from 'jquery';
+
+export default class VRWater extends Component {
+    state = {
+        character: character
+    }
+
+    // When this component mounts, grab the book with the _id of
+    // this.props.match.params.id e.g. localhost:3000/books/599dcb67f0f16317844583fc
+    // componentDidMount = () => {     API.getProfile(this.props.match.params.id)
+    //   .then(res => this.setState({ character: res.data }))         .catch(err =>
+    // console.log(err)); } Removes aframe classes when leaving this route
+    componentWillUnmount() {
+        $('html').removeClass('a-html');
+        // document.html.classList.remove('a-html');
+        document
+            .body
+            .classList
+            .remove('a-body');
+    }
+
+    render() {
+        // const { name } = this.props
+
+        return (
+            <a-scene fog="type: exponential; color: #FFF; density: 0.06;">
+
+                <a-entity camera="userHeight: 1.6"
+                            universal-controls="movementEnabled: false">
+                </a-entity>
+
+            
+                <a-ocean width="50" depth="50" density="40"></a-ocean>
+
+
+                <a-cylinder radius="1" height="50" color="#39BB82" position="0 -24.5 0"></a-cylinder>
+                <a-cylinder radius="1" height="50" color="#39BB82" position="5 -24.5 0"></a-cylinder>
+                <a-cylinder radius="1" height="50" color="#39BB82" position="-5 -24.5 0"></a-cylinder>
+
+
+                <a-entity light="type: hemisphere; color: #AAA; groundColor: #000000; intensity: 0.9;"></a-entity>
+                <a-entity light="type: ambient; color: #DC8874; intensity: 0.5;"></a-entity>
+            </a-scene>
+        );
+
+    }
+}
