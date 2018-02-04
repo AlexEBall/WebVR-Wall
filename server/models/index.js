218475c6b6147824138804e56e9a0937219b0484
Alex Edward Ball <ballalexedward@gmail.com>
2018-02-04 11:18:27 -0800

    fixes2

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-    Test: require("./test"),
-    Profile: require("./test")
-}
-=======
   Test: require("./test"),
   Profile: require("./profile")
 };
->>>>>>> 5e264f32603a12e0e631b7666828c9a8e67a9dfd
+
