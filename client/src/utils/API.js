5f9569507fcced4a4243065c48e7a008300afc64
Alex Edward Ball <ballalexedward@gmail.com>
2018-02-08 16:16:45 -0800

    import cors

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,13 @@
 import axios from "axios";
+import cors from 'cors';
 
 export default {
   // gets all profiles
   getProfiles: function() {
-    return axios.get("https://sheltered-badlands-98413.herokuapp.com/api");
+    return axios.get("https://sheltered-badlands-98413.herokuapp.com/api", cors());
   },
   // gets profile with given id
   getProfile: function(id) {
-return axios.get("https://sheltered-badlands-98413.herokuapp.com/api/" + id);
+return axios.get("https://sheltered-badlands-98413.herokuapp.com/api/" + id, cors());
   }
 };
