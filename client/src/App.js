3f3f77f355f1ae11db8cdbb6c86e5b6b04c52dc7
Alex Edward Ball <ballalexedward@gmail.com>
2018-02-09 17:57:23 -0800

    adding aframe scenes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import VRWorld from './pages/VRWorld';
 import VRGallery from './pages/VRGallery';
 import VRVideo from './pages/VRVideo';
 import VRPanoramic from './pages/VRPanoramic';
+import VRPanoramic2 from './pages/VRPanoramic2';
 import VRBeach from './pages/VRBeach';
 import VRHome from './pages/VRHome';
 
@@ -23,8 +24,8 @@ export default class App extends Component {
             <Route onUpdate={() => window.scrollTo(0, 0)} exact path="/motivationalWall" component={MotivationalWall} />
             <Route exact path="/VR-World/5a7bd4b214984f0014e74aef" component={VRPanoramic} />
             <Route exact path="/VR-World/5a7bd49f14984f0014e74aee" component={VRBeach} />
-            <Route exact path="/VR-World/5a7bd36e14984f0014e74aec" component={VRGallery} />
-            <Route exact path="/VR-World/5a7bd35d14984f0014e74aeb" component={VRHome} />
+            <Route exact path="/VR-World/5a7bd36e14984f0014e74aec" component={VRPanoramic2} />
+            <Route exact path="/VR-World/5a7bd35d14984f0014e74aeb" component={VRGallery} />
             <Route exact path="/VR-World/5a7bd34714984f0014e74aea" component={VRWorld} />
             <Route exact path="/VR-World/5a7bd2c914984f0014e74ae9" component={VRVideo} />
           </Switch>
